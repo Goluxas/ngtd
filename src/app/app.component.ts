f5f8003fc235be0138e4819ab13e29ad782348e0
Douglas Storz <douglasstorz@gmail.com>
2017-05-04 21:23:49 -0400

    Skeleton of main view and Task sturcture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { Task } from './task';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  master_list: Task[] = [];
+
+  new_task: Task = new Task(0);
+  auto_id: number = 1;
+
+  addTask(): void {
+    this.new_task.parseTags();
+    this.new_task.processSpecialTags();
+
+    this.master_list.push(this.new_task);
+
+    this.new_task = new Task(this.auto_id);
+    this.auto_id++;
+  }
+
+  writeMasterList(): void {
+    let outfile = 'masterlist.json';
+  }
 }
---
+++ src/app/task.ts
@@ -0,0 +1,99 @@
+export class Task {
+  constructor(
+    public id: number,
+    public summary?: string,
+    public notes?: string,
+
+    public captured_date?: Date,
+
+    public target_date?: Date,
+    public is_deadline?: boolean,
+    public is_start_date?: boolean,
+
+    public category?: string,
+
+    public tags?: string[],
+
+    public is_recurring?: boolean,
+    public frequency?: string,
+    public recurring_days?: string,
+  ) {
+    this.captured_date = new Date();
+    this.category = "inbox";
+  }
+
+  public parseTags(): void {
+    let tags: string[] = [];
+
+    let words = this.summary.trim().split(' ');
+
+    for (let word of words) {
+      if (word.length > 0 && word.charAt(0)) {
+        tags.push(word);
+      }
+    }
+
+    this.tags = tags;
+  }
+
+  public processSpecialTags(): void {
+    for (let tag of this.tags) {
+      if (tag.split('/').length >= 2) {
+        // date processing
+        let date_parts = tag.split('/');
+        let today = new Date();
+        let month, day, year = 0;
+        if (date_parts.length == 2) {
+          [month, day] = date_parts.map((part: string) => parseInt(part));
+          year = today.getFullYear(); // expecting a string for year
+        }
+        else if (date_parts.length == 3) {
+          [month, day, year] = date_parts.map((part: string) => parseInt(part));
+        }
+
+        // month-1 because javascript likes it's months 0-indexed for some reason
+        // the unary + operator converts them to numbers
+        this.target_date = new Date(+year, +month-1, +day);
+      }
+      else if (tag.indexOf("#days-") != -1) {
+        // days processing
+        this.recurring_days = tag.substr(6);
+      }
+      else {
+        // process keyword tags
+        switch(tag.toLowerCase()) {
+          case "#recurring": {
+            this.is_recurring = true;
+            break;
+          }
+
+          case "#daily": {
+            this.frequency = "daily";
+            break;
+          }
+
+          case "#weekly": {
+            this.frequency = "weekly";
+            break;
+          }
+
+          case "#montly": {
+            this.frequency = "montly";
+            break;
+          }
+
+          case "#starting-on": {
+            this.is_start_date = true;
+            break;
+          }
+
+          case "#deadline": {
+            this.is_deadline = true;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+}
